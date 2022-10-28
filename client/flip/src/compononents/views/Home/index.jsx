8069c7ad7720170da74097317ca538aa3ef8735d
Seth Zygarlicke <seth.zygarlicke@bestbuy.com>
2022-10-28 12:30:55 -0500

    Created flow for flipping between login and signup forms.

--- client/flip/src/compononents/views/Home/index.jsx
+++ client/flip/src/compononents/views/Home/index.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactCardFlip from 'react-card-flip';
 import LoginForm from '../../common/LoginForm';
+import SignupForm from '../../common/SignupForm';
+import SignupLoginToggle from '../../common/SignupLoginToggle';
 import styles from './Home.module.css';
 
 const Home = () => {
+    const [flipped, setFlipped] = useState(false);
+
+    const SIGNUP_TEXT = "Haven't Flipped yet? Sign up for free";
+    const LOGIN_TEXT = 'Already Flipped? Login instead';
+
+    const onClick = () => setFlipped(!flipped);
+
+    const toggleText = flipped ? LOGIN_TEXT : SIGNUP_TEXT;
+
     return (
         <div className={styles.homePage}>
             <div className={styles.splash}>
-                <div className={styles.logo}>
-                    Flip
-                </div>
-                <div className={styles.blurb}>
-                    Live. Laugh. Learn.
-                </div>
+                <div className={styles.logo}>Flip</div>
+                <div className={styles.blurb}>Live. Laugh. Learn.</div>
             </div>
-            <div className={styles.login}>
-                <LoginForm />
+            <div className={styles.loginSignup}>
+                <ReactCardFlip isFlipped={flipped} flipDirection="horizontal">
+                    <LoginForm />
+                    <SignupForm />
+                </ReactCardFlip>
+                <SignupLoginToggle text={toggleText} onClick={onClick} />
             </div>
         </div>
     );
