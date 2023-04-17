e0219bb55d85adbde4e996d728928316bc413237
Seth Zygarlicke <seth.zygarlicke@bestbuy.com>
2023-04-16 19:01:30 -0500

    Adding static assets handling for heroku.

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ app.use(function (req, res, next) {
   next();
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+};
+
 app.use(UserRoutes);
 app.use(DeckRoutes);
 
