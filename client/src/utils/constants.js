10bb4ee0e9f4762b55f586fa809ec8602f022813
Seth Zygarlicke <seth.zygarlicke@bestbuy.com>
2023-04-17 20:47:36 -0500

    Fixed error message on an invalid password in login.

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,28 +1,29 @@
 export const ERROR_MESSAGE = {
-  LOGIN: {
-    404: 'Huh, couldn\'t find any user with that email',
-    GENERIC: 'Something went wrong logging you in. Please try again'
-  },
-  SIGNUP: {
-    INVALID_EMAIL: 'Oops! That email doesn\'t look right',
-    PASSWORD_MISMATCH: 'Yikes! Your passwords don\'t match!'
-  },
-  CREATE_CARD: {
-    INCOMPLETE: 'You haven\'t finished filling out the card!',
-    DUPLICATE: 'You\'ve already added this card!'
-  },
-  FINISH_CARD: {
-    DOUBLE_CHECK: 'Don\'t forget to double check your cards!',
-    NO_NAME: 'This deck needs a name, bro!',
-    GENERIC: 'Something went wrong when finishing your deck, sorry!',
-  }
+    LOGIN: {
+        404: "Huh, couldn't find any user with that email",
+        GENERIC: 'Something went wrong logging you in. Please try again',
+        WRONG_PASSWORD: 'Ooof. Wrong password there, bud.',
+    },
+    SIGNUP: {
+        INVALID_EMAIL: "Oops! That email doesn't look right",
+        PASSWORD_MISMATCH: "Yikes! Your passwords don't match!",
+    },
+    CREATE_CARD: {
+        INCOMPLETE: "You haven't finished filling out the card!",
+        DUPLICATE: "You've already added this card!",
+    },
+    FINISH_CARD: {
+        DOUBLE_CHECK: "Don't forget to double check your cards!",
+        NO_NAME: 'This deck needs a name, bro!',
+        GENERIC: 'Something went wrong when finishing your deck, sorry!',
+    },
 };
 
 export const SIDES = {
-  FRONT: 'Front',
-  BACK: 'Back',
+    FRONT: 'Front',
+    BACK: 'Back',
 };
 
 export const LOCAL_STORAGE_KEYS = {
-  LOGGED_IN: 'userLoggedIn',
+    LOGGED_IN: 'userLoggedIn',
 };
