da5a8d3dff42d1e992105139426a8324775aa834
Seth Zygarlicke <75339531+ElderBass@users.noreply.github.com>
2023-05-05 20:43:30 -0500

    Moved the naming of a deck to the front of the flow. (#66)
    * is2gifidontfigurethiswebstormgitshitoutimapassoutduetorage.juststagemyfuckingchanges.
    
    * Starting to move the naming of the deck up the flow stack.
    
    * Successfully moved name deck form up the flow.
    
    ---------
    
    Co-authored-by: Laura Trebe <lauratrebe@Lauras-MacBook-Air.local>
    Co-authored-by: Seth Zygarlicke <seth.zygarlicke@bestbuy.com>

--- client/src/compononents/common/ActionCard/ActionCard.jsx
+++ client/src/compononents/common/ActionCard/ActionCard.jsx
@@ -2,31 +2,28 @@ import React from 'react';
 import styles from './ActionCard.module.css';
 
 const backgroundsMap = {
-  create: 'green',
-  stats: 'blue',
-  browse: 'purple'
+    create: 'green',
+    stats: 'blue',
+    browse: 'purple',
 };
 
 const ActionCard = ({ title, onClick }) => {
-  const backgroundClass = backgroundsMap[title.toLowerCase()];
+    const backgroundClass = backgroundsMap[title.toLowerCase()];
 
-  return (
-    <div
-      onClick={onClick}
-      className={`${styles.actionCard} ${styles[backgroundClass]}`}
-    >
-      <div className={styles.header}>
-        <div className={styles.spacer} />
-        <hr className={styles.line} />
-        <div className={styles.greyLines}>
-          <hr className={styles.greyLine} />
-          <hr className={styles.greyLine} />
-          <hr className={styles.greyLine} />
+    return (
+        <div onClick={onClick} className={`${styles.actionCard} ${styles[backgroundClass]}`}>
+            <div className={styles.header}>
+                <div className={styles.spacer} />
+                <hr className={styles.line} />
+                <div className={styles.greyLines}>
+                    <hr className={styles.greyLine} />
+                    <hr className={styles.greyLine} />
+                    <hr className={styles.greyLine} />
+                </div>
+                <span className={styles.text}>{title}</span>
+            </div>
         </div>
-        <span className={styles.text}>{title}</span>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ActionCard;
---
+++ client/src/compononents/common/NameDeckForm/NameDeckForm.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import styles from './NameDeckForm.module.css';
+
+const NameDeckForm = ({ deckName, setDeckName, submitName }) => {
+    const history = useHistory();
+
+    const [error, setError] = useState('');
+
+    const onChange = (e) => {
+        setError('');
+        setDeckName(e.target.value);
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (!deckName.length) {
+            setError('A deck has no name. This cannot be.');
+            return;
+        }
+        submitName(deckName);
+    };
+
+    return (
+        <div className={styles.nameDeckFormContainer}>
+            <div className={styles.error}>
+                {error && <p className={styles.errorMsg}>{error}</p>}
+            </div>
+            <form className={styles.nameDeckForm} onSubmit={onSubmit}>
+                <div className={styles.inputField}>
+                    <label className={styles.label} htmlFor="deckName">
+                        First, give the Deck a Name:
+                    </label>
+                    <hr className={styles.line} />
+                    <input
+                        className={styles.input}
+                        id="deckName"
+                        value={deckName}
+                        onChange={onChange}
+                    />
+                </div>
+                <div className={styles.actions}>
+                    <button className={styles.submitBtn} type="submit">
+                        Confirm Name
+                    </button>
+                    <button className={styles.cancelBtn} type="button" onClick={() => history.goBack()}>
+                        Cancel
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default NameDeckForm;
