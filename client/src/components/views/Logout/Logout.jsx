49a65f5c58c94ba88fa05884a310342830be7527
Seth Zygarlicke <seth.zygarlicke@bestbuy.com>
2023-06-04 19:29:05 -0500

    A commit totally unrelated to the socket.

--- client/src/components/views/Logout/Logout.jsx
+++ client/src/components/views/Logout/Logout.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import store from '../../../store';
-import * as UserActions from '../../../store/actions/user.js';
-import { LOCAL_STORAGE_KEYS } from '../../../utils/constants';
 import styles from './Logout.module.css';
 import LogoutForm from './LogoutForm';
+import { logout } from '../../../utils/logout';
 
 const Logout = () => {
     const history = useHistory();
 
     const onSubmit = async (e) => {
         e.preventDefault();
-        localStorage.setItem(LOCAL_STORAGE_KEYS.LOGGED_IN, false);
-        await store.dispatch(UserActions.logoutUser());
+        await logout();
         history.push('/');
     };
 
