6abbee6f3d16bd46d973ae9493a9951b3020cfb6
Seth Zygarlicke <seth.zygarlicke@bestbuy.com>
2023-06-27 20:19:41 -0500

    cleanup

--- client/src/components/common/ChatRoom/ChatRoom.jsx
+++ client/src/components/common/ChatRoom/ChatRoom.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-// import { trimEmail } from '../../../utils/helpers/emailHelpers';
 import UserResult from '../UserResult';
 import userResultStyles from '../UserResult/ChatResult.module.css';
 import styles from './ChatRoom.module.css';
 
 const ChatRoom = ({ room }) => {
     const { name, members = [] } = room;
-
-    // const membersString = members.map((member) => trimEmail(member.email)).join(', ');
 
     const onDeckClick = () => {};
 
@@ -19,10 +16,6 @@ const ChatRoom = ({ room }) => {
             <div className={styles.heading}>
                 <p style={{ margin: '8px' }}>Choose a Deck to Study</p>
             </div>
-            {/* <div className={styles.roomDeets}>
-                <p className={styles.currentMembers}>Current Members:</p>
-                <p className={styles.memberString}>{membersString}</p>
-            </div> */}
             <div className={styles.users}>
                 {members.length > 0 &&
                     members.map((member) => (
