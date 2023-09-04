f1b76e5053d2077822db8a81939bae05fdaafd42
Seth Zygarlicke <75339531+ElderBass@users.noreply.github.com>
2023-09-04 13:49:34 -0500

    Choosing a new host when the host leaves a room. (#87)
    * Choosing a new host when the host leaves a room.
    
    * Cleanup.
    
    ---------
    
    Co-authored-by: Seth Zygarlicke <seth.zygarlicke@bestbuy.com>

--- client/src/components/common/RoomList/RoomList.jsx
+++ client/src/components/common/RoomList/RoomList.jsx
@@ -4,22 +4,19 @@ import styles from './RoomList.module.css';
 
 const RoomList = ({ rooms, email }) => {
     return (
-        <div className={styles.roomListContainer}>
-            <div className={styles.header}>
-                Join a Room
-            </div>
-            <div className={styles.roomList}>
-                {rooms.length ? (
-                    rooms.map((room) => (
-                        <RoomListItem
-                            key={room.id}
-                            room={room}
-                            email={email}
-                        />
-                    ))
-                ) : (
-                    <p>There are currently no chat rooms</p>
-                )}
+        <div className={styles.roomListContent}>
+            <h2 className={styles.flipWithFriends}>Flip with Friends</h2>
+            <div className={styles.roomListContainer}>
+                <div className={styles.header}>Join a Room</div>
+                <div className={styles.roomList}>
+                    {rooms.length ? (
+                        rooms.map((room) => (
+                            <RoomListItem key={room.id} room={room} email={email} />
+                        ))
+                    ) : (
+                        <p>There are currently no chat rooms</p>
+                    )}
+                </div>
             </div>
         </div>
     );
